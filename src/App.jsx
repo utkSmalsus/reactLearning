2a32db39fc46ba2f8775d9e05c91c6775f648fde
U_K_R_H <apple@MacBook-Air.local>
2026-02-17 21:58:19 +0530

    useState form

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import Header from './satyComponents/Header';
 import { Cardpanel, Blogpanel } from './satyComponents/Card';
 import Footer from './satyComponents/footer';
 import { blog } from './Data/Blog';
-import Newcount  from './satyComponents/Newconut';
+import Newcount from './satyComponents/Newconut';
+import UserForm from './components/UserForm';
 
 
 
@@ -17,52 +18,36 @@ function App() {
   //  let Name = "Html CSS JS";
   //  let Title = "React developer";
   //  let Desc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.";
-   let casrdData = {
-    Name: "Html CSS JS and React",
-    Title: "React developer",
-    Desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate."
-
-  }
-  let footerinfo = {
-    Copy: "Copyright @ 2024",
-
-  }
+ 
 
 
   return (
     <>
-    <Header heading="Reacat program" subheading="my first program"/> {/* this is static data(custom attributes)*/}
-  {/* <Getinput></Getinput>
+      <Header heading="Reacat program" subheading="my first program" /> {/* this is static data(custom attributes)*/}
+      {/* <Getinput></Getinput>
     <Countersk></Countersk> 
    <Parent />*/}
-   
-   {/* <Counter /> */}
-   <Container>
-    <Row>
-          <Cardpanel casrdData={casrdData} /> 
-          <Cardpanel casrdData={casrdData} /> 
-               <Cardpanel casrdData={casrdData} /> 
-                    <Cardpanel casrdData={casrdData} /> 
-    </Row>
- 
-   </Container>
-   <Container className='my-5'>
-    <Row> 
-      {blog.map((item,index)=>{
-        return(
-       <Blogpanel blogitem={item} key={index} /> 
-        )
-      })
-      }
-    
-    </Row>
-   </Container>
-   
-   <Minitodo />
-   <Evenandoddck />
-   <Newcount />
 
-    <Footer footerinfo={footerinfo}><h6>Design by saty</h6></Footer>
+      {/* <Counter /> */}
+     
+      {/* <Container className='my-5'>
+        <Row>
+        {
+          blog.map((item, index) => {
+            return (
+              <Blogpanel blogitem={item} key={index} />
+            )
+          })
+      }
+
+        </Row>
+      </Container>
+
+      <Minitodo />
+      <Evenandoddck />
+      <Newcount /> */}
+
+      <UserForm />
 
     </>
   )
